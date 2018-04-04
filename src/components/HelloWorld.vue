c8d0be0c04952428336cc4ae24e1269a50303a5c
zhouming <zhouming@gaodun.com>
2018-04-04 09:49:13 +0800

    new

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <text class="message">Now, let's use Vue.js to build your Weex app.</text>
+  <text>{{text}}</text>
+  <input type="text" placeholder="Input Text" class="input" :autofocus=true value="" @change="onchange" @input="oninput"/>
+  <div class="btn" @click="btnClick"><text class="btn-text">点击11</text></div>
+</div> 
+</template>
+<style scoped>
+.btn{
+  padding: 5px 10px;
+  background-color: #ff0000;
+  
+}
+.btn-text{
+  color: #ffffff;
+}
+</style>
+<script>
+var modal = weex.requireModule('modal');
+export default {
+  data(){
+    return {
+      text:"111"
+    }
+  },
+  methods:{
+    btnClick( event ){
+      modal.alert({
+        message: 'This is a alert',
+        duration: 0.3
+    }, ()=> {
+      this.text = "sss";
+    })
+      
+    }
+  }
+}
+</script>
